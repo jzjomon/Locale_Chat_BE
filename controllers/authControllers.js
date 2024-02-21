25e1ecb1d03101a93336101199aad1702c5e7ac9
jomon jose <jzjomon@gmail.com>
2024-02-21 10:07:53 +0530

    updated

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -6,7 +6,7 @@ export const signUp = async (req, res) => {
     try {
         const { firstname, lastname, email, location, password } = req.body.details;
         if (firstname && lastname && email && location && password) {
-            const exist = await USER.findOne({ email: email }); 
+            const exist = await USER.findOne({ email: email });
             if (exist) return res.status(400).json({ message: "allready have an account !" });
             bcrypt.hash(password, parseInt(process.env.SALT), async (err, hash) => {
                 if (err) return res.status(400).json({ message: "hashing error" });
@@ -26,16 +26,19 @@ export const signIn = async (req, res) => {
     try {
         const { email, password } = req.body;
         if (!email || !password) return res.status(400).json({ message: "validation failed" });
-        const exist = await USER.findOne({ email });
-        if (!exist) return res.status(400).json({ message: "You don't have an account. Please sign up !" });
-        bcrypt.compare(password, exist.password, (err, result) => {
-            if (err) return res.status(400).json({ message: "bcrypt compare failed" });
-            if (!result) return res.status(400).json({ message: "incorrect password" });
-            jwt.sign({ id: exist._id }, process.env.JWT_SECRET, (err, token) => {
-                if (err) return res.status(400).json({ message: "Something went wrong !" });
-                res.status(200).json({ message: "success", data: exist, token });
+        USER.findOne({ email }).then((result) => {
+            bcrypt.compare(password, result.password, (err, result) => {
+                if (err) return res.status(400).json({ message: "bcrypt compare failed" });
+                if (!result) return res.status(400).json({ message: "incorrect password" });
+                jwt.sign({ id: exist._id }, process.env.JWT_SECRET, (err, token) => {
+                    if (err) return res.status(400).json({ message: "Something went wrong !" });
+                    res.status(200).json({ message: "success", data: exist, token });
+                })
             })
-        })         
+        }).catch((err) => {
+            res.status(400).json({ message: "You don't have an account. Please sign up !" });
+        });
+
     } catch (error) {
         res.status(500).json({ message: "Something went wrong" })
 
