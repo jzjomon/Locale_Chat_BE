c1bb1e0e80cf556d37022ce23ef76d2912d2cb20
jomon jose <jzjomon@gmail.com>
2024-03-20 14:16:55 +0530

    updated

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const expressServer = app.listen(process.env.PORT, () => {
 
 const io = new Server(expressServer, {
     cors: {
-        origin: `${process.env.CLIENT_URL}`
+        origin : "*",
     }
 })
 
